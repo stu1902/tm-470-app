f9aa308e9fc5297a07c7d1c337eaad0259c3f14c
stu1902 <stuart1902@gmail.com>
2022-05-14 18:00:04 +0100

    Weekly commit

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,11 @@
 package io.ionic.starter;
+import com.hemangkumar.capacitorgooglemaps.CapacitorGoogleMaps;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          registerPlugin(CapacitorGoogleMaps.class);
+}
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -16,6 +16,7 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">TM470 App Details Page</IonTitle>
           </IonToolbar>
         </IonHeader>
+
         <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
